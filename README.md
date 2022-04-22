d370a36d0a24253d6259d3821f966754771c4252
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-04-22 19:37:09 -0400

    --all

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Most of this code is derived from a tutorial I followed along, as the Javascript
 use of the "once : true" method in event listeners, designing characters using transform in CSS and not relying on fonts, iterating through the every method in javascript to search for win or lose patterns in simple games like this, and using psuedo elements "::before" and "::after" to implement certain hover effects through CSS
 
 Update 1: Added a winning combination that was missing, and added text that show's whose turn it is
+
+Update 2: Added AI that plays against itself
--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
+let X_CLASS = 'x'
+let CIRCLE_CLASS = 'circle'
+let currentClass = X_CLASS
 const restartButton = document.getElementById('restartButton')
+const CELL_1 = document.getElementById('1')
+const CELL_2 = document.getElementById('2')
+const CELL_3 = document.getElementById('3')
+const CELL_4 = document.getElementById('4')
+const CELL_5 = document.getElementById('5')
+const CELL_6 = document.getElementById('6')
+const CELL_7 = document.getElementById('7')
+const CELL_8 = document.getElementById('8')
+const CELL_9 = document.getElementById('9')
+
 const WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5], 
@@ -12,6 +23,88 @@ const WINNING_COMBINATIONS = [
     [2, 5, 8]
 ]
 
+const ALL_SPOTS = [
+    0, 1, 2, 
+    3, 4, 5, 
+    6, 7, 8 
+]
+
+
+let removedValue; // The empty array which will have a random spot on the board pushed into it.
+
+function removeAvailableSpot() {
+
+    let result = Math.floor(Math.random() * ALL_SPOTS.length); 
+    removedValue = ALL_SPOTS[result]
+    ALL_SPOTS.splice(result, 1)
+    return removedValue
+}
+
+
+
+function returnNumber() {
+    setInterval(1000)
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    removeAvailableSpot();
+     let newSpot = removedValue
+    if (newSpot == 0) {
+        cell = CELL_1
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 1) {
+        cell = CELL_2
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 2) {
+        cell = CELL_3
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 3) {
+        cell = CELL_4
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 4) {
+        cell = CELL_5
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 5) {
+        cell = CELL_6
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 6) {
+        cell = CELL_7
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 7) {
+        cell = CELL_8
+        placeMark(cell, currentClass)
+        
+    } else if (newSpot == 8) {
+        cell = CELL_9
+        placeMark(cell, currentClass)
+        
+    }
+   
+   
+}
+
+
+function playAutomatedGame() {
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    returnNumber();
+
+    if (checkWin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
+    }
+}
+
+
+
 const winningMessageTextElement = document.querySelector('[data-winner-text]')
 const winningMessageElement = document.getElementById('winnerMessage')
 
@@ -19,11 +112,14 @@ const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('gameBoard')
 const turner = document.getElementById('turn-name')
 let circleTurn
+/*
 startGame();
 
 
 restartButton.addEventListener('click', startGame)
 
+*/
+/*
 function startGame() {
     circleTurn = false;
     cellElements.forEach(cell => {
@@ -35,9 +131,9 @@ function startGame() {
     setBoardHoverClass()
     winningMessageElement.classList.remove('show')
 }
+*/
 
-
-
+/*
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
@@ -51,6 +147,11 @@ function handleClick(e) {
         setBoardHoverClass()
     }
 }
+*/
+
+
+
+
 
 function isDraw() {
     return [...cellElements].every(cell => {
@@ -61,6 +162,8 @@ function isDraw() {
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
+
+
 
 function swapTurns() {
     circleTurn = !circleTurn
@@ -89,6 +192,8 @@ function checkWin(currentClass) {
     })
 }
 
+
+
 function endGame(draw) {
     if (draw) {
         winningMessageTextElement.innerText = 'Draw!'
@@ -97,3 +202,40 @@ function endGame(draw) {
     }
     winningMessageElement.classList.add('show')
 }
+
+/*
+setTimeout(returnNumber, 1000)
+setTimeout(returnNumber, 2000)
+setTimeout(returnNumber, 3000)
+setTimeout(returnNumber, 4000)
+setTimeout(returnNumber, 5000)
+setTimeout(returnNumber, 7000)
+setTimeout(returnNumber, 8000)
+setTimeout(returnNumber, 9000)
+*/
+
+
+
+/*
+  
+  for (let i = 1; i < 10; i++) {
+    setInterval(returnNumber(), 1000);
+}
+*/
+
+
+
+for (let i=0; i<9; i++) { 
+    task(i);
+ }
+   
+ function task(i) {
+   setTimeout(function() {
+       playAutomatedGame()
+   }, 1000 * i);
+
+ }
+ 
+
+
+
